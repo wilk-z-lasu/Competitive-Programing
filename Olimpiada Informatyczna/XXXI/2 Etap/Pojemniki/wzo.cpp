dff5e858aa4be419b6b23569c38c59990459a82f
Mateusz Woloszyk <themateuszwoloszyk@gmail.com>
2025-09-07 17:08:32 +0200

    adding solution for Pojemniki

--- Olimpiada Informatyczna/XXXI/2 Etap/Pojemniki/wzo.cpp
+++ Olimpiada Informatyczna/XXXI/2 Etap/Pojemniki/wzo.cpp
@@ -1,4 +1,5 @@
 // Mateusz Woloszyk
+//Solution for 100/100 points in O(n log n)
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -14,7 +15,8 @@ constexpr int maxn=1e6+7;
 constexpr int inf=1e18+7;
 
 int n,k;
-multiset<int>S;
+int a, a_sum;
+set<pi>S;
 int32_t main()
 {
 	ios_base::sync_with_stdio(0);
@@ -23,9 +25,33 @@ int32_t main()
     cin>>n>>k;
     for(int i=1;i<=n;++i)
     {
-        int a;
         cin>>a;
-        S.insert(a);
+        a_sum += a;
+        S.insert({a,i});
     }
+    if(a_sum > n*k)
+    {
+        cout<<"NIE";
+        return 0;
+    }
+    cout<<"TAK\n";
+    while(!S.empty())
+    {
+        int amount = (*S.begin()).f;
+        int idx = (*S.begin()).s;
+        S.erase(S.begin());
 
+        auto fill = S.upper_bound({k-amount,inf});
+        if(fill == S.end())
+            cout<<1<<' '<<idx<<' '<<amount<<'\n';
+        else
+        {
+            pi add = *fill;
+            S.erase(fill);
+            cout<<2<<' '<<idx<<' '<<amount<<' '<<add.s<<' '<<k-amount<<'\n';
+            add.f -= k-amount;
+            S.insert(add);
+        }
+    }
+    return 0;
 }
